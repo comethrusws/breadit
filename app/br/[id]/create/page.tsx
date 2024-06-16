9517b4cdbc51b0cfda23b3c302d27d81923d9f0c
comethrusws <basabjha@naver.com>
2024-06-17 03:29:39 +0545

    Commit #6: Post Feature fix (finally!)

--- app/br/[id]/create/page.tsx
+++ app/br/[id]/create/page.tsx
@@ -1,169 +1,145 @@
-import { CreatePostCard } from "@/app/components/CreatePostCard";
-import Pagination from "@/app/components/Pagination";
-import { PostCard } from "@/app/components/PostCard";
-import { SubDescriptionForm } from "@/app/components/SubDescriptionForm";
-import prisma from "@/app/lib/db";
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Cake, FileQuestion } from "lucide-react";
+"use client";
+
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
+import pfp from "../../../../public/pfp.png";
+import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import { unstable_noStore as noStore } from "next/cache";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Text, Video } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { TipTapEditor } from "@/app/components/TipTabEditor";
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import { UploadDropzone } from "@/app/components/Uploadthing";
+import { useState } from "react";
+import { createPost } from "@/app/actions";
+import { JSONContent } from "@tiptap/react";
 
-async function getData(name: string, searchParam: string) {
-  noStore();
-  const [count, data] = await prisma.$transaction([
-    prisma.post.count({
-      where: {
-        subName: name,
-      },
-    }),
+const rules = [
+  {
+    id: 1,
+    text: "Remember the human",
+  },
+  {
+    id: 2,
+    text: "Behave like you would in real life",
+  },
+  {
+    id: 3,
+    text: "Look for the original source of content",
+  },
+  {
+    id: 4,
+    text: "Search for duplication before posting",
+  },
+  {
+    id: 5,
+    text: "Read the community guidlines",
+  },
+];
 
-    prisma.subbreadit.findUnique({
-      where: {
-        name: name,
-      },
-
-      select: {
-        name: true,
-        createdAt: true,
-        description: true,
-        userId: true,
-        posts: {
-          take: 10,
-          skip: searchParam ? (Number(searchParam) - 1) * 10 : 0,
-
-          select: {
-            Comment: {
-              select: {
-                id: true,
-              },
-            },
-            title: true,
-            imageString: true,
-            id: true,
-            textContent: true,
-            Vote: {
-              select: {
-                userId: true,
-                voteType: true,
-              },
-            },
-            User: {
-              select: {
-                userName: true,
-              },
-            },
-          },
-        },
-      },
-    }),
-  ]);
-
-  return { data, count };
-}
-
-export default async function SubreaditRoute({
+export default function CreatePostRoute({
   params,
-  searchParams,
 }: {
   params: { id: string };
-  searchParams: { page: string };
 }) {
-  const { data, count } = await getData(params.id, searchParams.page);
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  const [imageUrl, setImageUrl] = useState<null | string>(null);
+  const [json, setJson] = useState<null | JSONContent>(null);
+  const [title, setTitle] = useState<null | string>(null);
+
+  const createPostReddit = createPost.bind(null, { jsonContent: json });
   return (
-    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10">
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
       <div className="w-[65%] flex flex-col gap-y-5">
-        <CreatePostCard />
+        <h1 className="font-semibold">
+          Subreddit:{" "}
+          <Link href={`/r/${params.id}`} className="text-primary">
+            r/{params.id}
+          </Link>
+        </h1>
+        <Tabs defaultValue="post" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="post">
+              <Text className="h-4 w-4 mr-2" /> Post
+            </TabsTrigger>
+            <TabsTrigger value="image">
+              <Video className="h-4 w-4 mr-2" />
+              Image & Video
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="post">
+            <Card>
+              <form action={createPostReddit}>
+                <input
+                  type="hidden"
+                  name="imageUrl"
+                  value={imageUrl ?? undefined}
+                />
+                <input type="hidden" name="subName" value={params.id} />
+                <CardHeader>
+                  <Label>Title</Label>
+                  <Input
+                    required
+                    name="title"
+                    placeholder="Title"
+                    value={title ?? ""}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
 
-        {data?.posts.length === 0 ? (
-          <div className="flex min-h-[300px] flex-col justify-center items-center rounded-md border border-dashed p-8 text-center">
-            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
-              <FileQuestion className="h-10 w-10 text-primary" />
-            </div>
-
-            <h2 className="mt-6 text-xl font-semibold">
-              No posts have been created
-            </h2>
-          </div>
-        ) : (
-          <>
-            {data?.posts.map((post) => (
-              <PostCard
-                key={post.id}
-                id={post.id}
-                imageString={post.imageString}
-                subName={data.name}
-                commentAmount={post.Comment.length}
-                title={post.title}
-                userName={post.User?.userName as string}
-                jsonContent={post.textContent}
-                voteCount={post.Vote.reduce((acc, vote) => {
-                  if (vote.voteType === "UP") return acc + 1;
-                  if (vote.voteType === "DOWN") return acc - 1;
-
-                  return acc;
-                }, 0)}
-              />
-            ))}
-
-            <Pagination totalPages={Math.ceil(count / 10)} />
-          </>
-        )}
+                  <TipTapEditor setJson={setJson} json={json} />
+                </CardHeader>
+                <CardFooter>
+                  <SubmitButton text="Create Post" />
+                </CardFooter>
+              </form>
+            </Card>
+          </TabsContent>
+          <TabsContent value="image">
+            <Card>
+              <CardHeader>
+                {imageUrl === null ? (
+                  <UploadDropzone
+                    className="ut-button:bg-primary ut-button:ut-readying:bg-primary/50 ut-label:text-primary ut-button:ut-uploading:bg-primary/50 ut-button:ut-uploading:after:bg-primary"
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                      setImageUrl(res[0].url);
+                    }}
+                    onUploadError={(error: Error) => {
+                      alert("Error");
+                    }}
+                  />
+                ) : (
+                  <Image
+                    src={imageUrl}
+                    alt="uploaded image"
+                    width={500}
+                    height={400}
+                    className="h-80 rounded-lg w-full object-contain"
+                  />
+                )}
+              </CardHeader>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
-
       <div className="w-[35%]">
-        <Card>
-          <div className="bg-muted p-4 font-semibold">About Community</div>
-          <div className="p-4">
-            <div className="flex items-center gap-x-3">
-              <Image
-                src={`https://avatar.vercel.sh/${data?.name}`}
-                alt="Image of subreddit"
-                width={60}
-                height={60}
-                className="rounded-full h-16 w-16"
-              />
-              <Link href={`/br/${data?.name}`} className="font-medium">
-                r/{data?.name}
-              </Link>
-            </div>
-            {user?.id === data?.userId ? (
-              <SubDescriptionForm
-                description={data?.description}
-                subName={params.id}
-              />
-            ) : (
-              <p className="text-sm font-normal text-secondary-foreground mt-2">
-                {data?.description}
-              </p>
-            )}
+        <Card className="flex flex-col p-4">
+          <div className="flex items-center gap-x-2">
+            <Image className="h-10 w-10" src={pfp} alt="pfp" />
+            <h1 className="font-medium">Posting to Reddit</h1>
+          </div>
+          <Separator className="mt-2" />
 
-            <div className="flex items-center gap-x-2 mt-4">
-              <Cake className="h-5 w-5 text-muted-foreground" />
-              <p className="text-muted-foreground font-medium text-sm">
-                Created:{" "}
-                {new Date(data?.createdAt as Date).toLocaleDateString("en-us", {
-                  weekday: "long",
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </p>
-            </div>
-
-            <Separator className="my-5" />
-            <Button asChild className="rounded-full w-full">
-              <Link
-                href={user?.id ? `/br/${data?.name}/create` : "/api/auth/login"}
-              >
-                Create Post
-              </Link>
-            </Button>
+          <div className="flex flex-col gap-y-5 mt-5">
+            {rules.map((item) => (
+              <div key={item.id}>
+                <p className="text-sm font-medium">
+                  {item.id}. {item.text}
+                </p>
+                <Separator className="mt-2" />
+              </div>
+            ))}
           </div>
         </Card>
       </div>
--- app/br/[id]/page.tsx
+++ app/br/[id]/page.tsx
@@ -1,71 +1,172 @@
-import { updateDescription } from "@/app/actions";
-import {SubDescriptionForm} from "@/app/components/SubDescriptionForm";
-import { SaveButton } from "@/app/components/SubmitButtons";
+import { CreatePostCard } from "@/app/components/CreatePostCard";
+import Pagination from "@/app/components/Pagination";
+import { PostCard } from "@/app/components/PostCard";
+import { SubDescriptionForm } from "@/app/components/SubDescriptionForm";
 import prisma from "@/app/lib/db";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Cake } from "lucide-react";
+import { Cake, FileQuestion } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { unstable_noStore as noStore } from "next/cache";
 
-async function getData(name: string){
-    const data = await prisma.subbreadit.findUnique({
-        where:{
-            name: name,
+async function getData(name: string, searchParam: string) {
+  noStore();
+  const [count, data] = await prisma.$transaction([
+    prisma.post.count({
+      where: {
+        subName: name,
+      },
+    }),
+
+    prisma.subbreadit.findUnique({
+      where: {
+        name: name,
+      },
+
+      select: {
+        name: true,
+        createdAt: true,
+        description: true,
+        userId: true,
+        posts: {
+          take: 10,
+          skip: searchParam ? (Number(searchParam) - 1) * 10 : 0,
+
+          select: {
+            Comment: {
+              select: {
+                id: true,
+              },
+            },
+            title: true,
+            imageString: true,
+            id: true,
+            textContent: true,
+            Vote: {
+              select: {
+                userId: true,
+                voteType: true,
+              },
+            },
+            User: {
+              select: {
+                userName: true,
+              },
+            },
+          },
         },
-        select:{
-            name: true,
-            createdAt: true,
-            description: true,
-            userId: true,
-        }
-    });
-    return data;
+      },
+    }),
+  ]);
+
+  return { data, count };
 }
 
-export default async function SubbreaditRoute({params}:{params:{id: string}}){
-    const data= await getData(params.id);
-    const {getUser}= getKindeServerSession();
-    const user = await getUser();
+export default async function SubRedditRoute({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { page: string };
+}) {
+  const { data, count } = await getData(params.id, searchParams.page);
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <CreatePostCard />
 
-    return(
-        <div className="max-w-[80%] mx-auto flex gap-x-10 mt-4">
-            <div className="w-[65%] flex flex-col gap-y-5">
-                hello from post
+        {data?.posts.length === 0 ? (
+          <div className="flex min-h-[300px] flex-col justify-center items-center rounded-md border border-dashed p-8 text-center">
+            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
+              <FileQuestion className="h-10 w-10 text-primary" />
             </div>
 
-            <div className="w-[35%] ">
-                <Card>
-                    <div className="bg-muted font-semibold p-4 ">About Community</div>
-                    <div className="p-4">
-                        <div className="flex items-center gap-x-3">
-                            <Image className="rounded-full h-16 w-16" src={`https://avatar.vercel.sh/${data?.name}`} width={60} height={60} alt="image of Subbreadit"/>
-                            <Link className="font-medium" href={`/br/${data?.name}`}>br/{data?.name}</Link>
-                        </div>
+            <h2 className="mt-6 text-xl font-semibold">
+              No post have been created
+            </h2>
+          </div>
+        ) : (
+          <>
+            {data?.posts.map((post) => (
+              <PostCard
+                key={post.id}
+                id={post.id}
+                imageString={post.imageString}
+                subName={data.name}
+                commentAmount={post.Comment.length}
+                title={post.title}
+                userName={post.User?.userName as string}
+                jsonContent={post.textContent}
+                voteCount={post.Vote.reduce((acc, vote) => {
+                  if (vote.voteType === "UP") return acc + 1;
+                  if (vote.voteType === "DOWN") return acc - 1;
 
-                        {user?.id === data?.userId?(
-                            <SubDescriptionForm
-                            description={data?.description}
-                            subName={params.id}
-                          />
-                        ):(
-                            <p className="text-secondary-foreground text-sm font-normal mt-2">{data?.description}</p>
-                        )}
+                  return acc;
+                }, 0)}
+              />
+            ))}
 
-                        <div className=" flex items-center gap-x-2 mt-4">
-                            <Cake className="h-5 w-5 text-muted-foreground text-xs"/>
-                            <p className="text-xs text-muted-foreground">Created: {new Date(data?.createdAt as Date).toLocaleDateString('en-us', {weekday:'long', year:'numeric', month:'short'})}</p>
-                        </div>
+            <Pagination totalPages={Math.ceil(count / 10)} />
+          </>
+        )}
+      </div>
 
-                        <Separator className="my-5"/>
-                        <Button asChild className="rounded-full w-full">
-                            <Link href={user?.id ? `/br/${data?.name}/create` : '/api/auth/login'}> Create Post</Link>
-                        </Button>
-                    </div>
-                </Card>
+      <div className="w-[35%]">
+        <Card>
+          <div className="bg-muted p-4 font-semibold">About Community</div>
+          <div className="p-4">
+            <div className="flex items-center gap-x-3">
+              <Image
+                src={`https://avatar.vercel.sh/${data?.name}`}
+                alt="Image of subreddit"
+                width={60}
+                height={60}
+                className="rounded-full h-16 w-16"
+              />
+              <Link href={`/br/${data?.name}`} className="font-medium">
+                r/{data?.name}
+              </Link>
             </div>
-        </div>
-    )
+            {user?.id === data?.userId ? (
+              <SubDescriptionForm
+                description={data?.description}
+                subName={params.id}
+              />
+            ) : (
+              <p className="text-sm font-normal text-secondary-foreground mt-2">
+                {data?.description}
+              </p>
+            )}
+
+            <div className="flex items-center gap-x-2 mt-4">
+              <Cake className="h-5 w-5 text-muted-foreground" />
+              <p className="text-muted-foreground font-medium text-sm">
+                Created:{" "}
+                {new Date(data?.createdAt as Date).toLocaleDateString("en-us", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </p>
+            </div>
+
+            <Separator className="my-5" />
+            <Button asChild className="rounded-full w-full">
+              <Link
+                href={user?.id ? `/br/${data?.name}/create` : "/api/auth/login"}
+              >
+                Create Post
+              </Link>
+            </Button>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
